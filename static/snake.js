9c17ba23497fd843002f443bacfaafca36ff3ab8
flyste <42930850+flyste@users.noreply.github.com>
2025-02-10 16:16:30 +0000

    Update snake.js
    added wall

--- static/snake.js
+++ static/snake.js
@@ -14,6 +14,7 @@ class snake{
 	this.body[0] = createVector(x, y);
 	this.eaten = 0;
 	this.name = name;
+	this.colourSeeds = [random(0, 255), random(0, 255), random(0, 255)];
 	for(var k = 1; k < this.length; k++){
 		this.body[k] = createVector(x, y);
 	}
@@ -26,7 +27,7 @@ class snake{
     
     //print(floor(d), this.size / 2 + 10);
     
-    if(floor(d) <= this.size / 2 + size){
+    if(floor(d) <= this.size / 2 + size / 2){
 		this.eaten += size;
 		if(this.eaten / 10 > 4){
 			this.grow();
@@ -48,9 +49,10 @@ class snake{
   
   
   
-  show(){
+  show(colours){
     
 	
+	//text(this.name + " " + this.body[0].x + ":" + this.body[0].y, this.body[0].x - 30, this.body[0].y + 30);
 	text(this.name, this.body[0].x - 30, this.body[0].y + 30);
     for(var j = this.length - 1; j >= 0 ; j--){
 		//console.log(this.size, this.length, this.body.length);
@@ -58,11 +60,12 @@ class snake{
 		push();
 		noStroke();
 		if( j % 2 === 0){
-			fill(255, 0, 255);
+			fill(this.colourSeeds[0], this.colourSeeds[1], this.colourSeeds[2]);
 		}
 		else {
-			fill(255, 0, 0);
+			fill(this.colourSeeds[0], 0, 0);
 		}
+		blendMode(DIFFERENCE);
 		ellipse(this.body[j].x, this.body[j].y, this.size);
 		pop();
     }
@@ -91,10 +94,22 @@ class snake{
 	  
 	var pos = createVector(x,y);
     var p = this.body[0].dist(pos);
-	
-	if(floor(p) <= (this.size / 2 + size)){
+	console.log(x, y);
+	if(floor(p) <= (this.size / 2 + size / 2)){
 		console.log("hit");
 		return(true);		
+	}
+	else{
+		return(false);
+	}
+	  
+  }
+  
+  hitWall(){
+	  
+	var radius = this.size / 2;  
+	if(this.body[0].x < (-800 + radius) || this.body[0].x > (800 * 3 - radius) || this.body[0].y < (-800 + radius) || this.body[0].y > (800 * 3 - radius)){
+		return(true);
 	}
 	else{
 		return(false);
